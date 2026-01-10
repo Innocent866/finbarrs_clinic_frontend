778127a1860a424d4d65b5263b9ed37bc2bf3f69
Innocent866 <igoldima@gmail.com>
2026-01-10 11:38:21 +0100

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+  base: "/",
+  server: mode === "development" ? {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://finbarrs-clinic-backend.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  } : undefined,
+}));
